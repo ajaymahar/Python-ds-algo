8ef88eb147d27a08d2c7eda195d3c99e85719d33
Ajay Kumar Mahar <ajay.mhar@gmail.com>
2019-03-08 00:30:02 +0530

    updating test case structure, switched to pytest over unittest lib.

--- tests/test_anagrams.py
+++ tests/test_anagrams.py
@@ -1,13 +1,62 @@
+'''
+1) rearrange the word ---> “gamanra”
+testcase1 = [“anagram”, “gamanra”, True]
+2) rearrange and change capitalization ---> “GraMana”
+I need two tests to verify that capitalization is ignored for both input strings
+testcase2a= [“anagram”, “GraMana”, True]
+testcase2b= [“AnAgrAm”, “gramana”, True]
+3) swap only 2 characters
+a) at the start “naagram”
+testcase3a= [“anagram”,“naagram”, True]
+b) in the middle “anargam”
+testcase3b= [“anagram”, “anargam”, True]
+c) at the end “anagrma”
+testcase3c= [“anagram”, “anagrma”, True]
+4) test the shortest possible anagram (containing only two characters)
+testcase4 = [“an”, “na”, True]
+
+another examples:
+
+Jim Morrison = Mr Mojo Risin
+Damon Albarn = Dan Abnormal
+George Bush = He bugs Gore
+Clint Eastwood = Old West action
+Ronald Reagan = A darn long era
+Elvis = Lives
+Madonna Louise Ciccone = One cool dance musician
+
+-------------------------------------------------------------------------------------
+positive_testcases = {"Ronald Reagan":"A darn long era", "Elvis":"Lives", "Madonna Louise Ciccone":"One cool dance musician" ,"Jim Morrison":"Mr Mojo Risin","Damon Albarn":"Dan Abnormal", "George Bush":"He bugs Gore", "Clint Eastwood":"Old West action", "anagram":"gamanra", "anagram":"GraMana", "AnAgrAm":"gramana", "anagram": "naagram", "anagram":"anargam", "anagram": "anagrma", "an":"na"}
+--------------------------------------------------------------------------------------
+
+For simplicity, I keep my base word “anagram”
+1) a completely different word
+a) of same length
+testcase1a = [“anagram”, “wxdcfvb”, False]
+b) of different length
+testcase1b = [“anagram”, “python”, False]
+
+-------------------------------------------------------------------------------------
+negative_testcases = {"anagram":"wxdcfvb", "anagram":"python"}
+--------------------------------------------------------------------------------------
+
+'''
+
 from solutions.Arrays import anagrams
-import unittest
 
-class TestAnagrams(unittest.TestCase):
-    def test_anagram_v1(self):
-        test_data = {'god':'Dog', 'Public relations':'crap built on lies', 'clint east wood':'old west action'}
-        for firstString, secondString in test_data.items():
-            result = anagrams.anagram_v1(firstString,secondString)
-            self.assertTrue(result, msg=None)
+positive_testcases = {"Ronald Reagan":"A darn long era", "Elvis":"Lives", "Madonna Louise Ciccone":"One cool dance musician" ,"Jim Morrison":"Mr Mojo Risin","Damon Albarn":"Dan Abnormal", "George Bush":"He bugs Gore", "Clint Eastwood":"Old West action", "anagram":"gamanra", "anagram":"GraMana", "AnAgrAm":"gramana", "anagram": "naagram", "anagram":"anargam", "anagram": "anagrma", "an":"na"}
+negative_testcases = {"anagram":"wxdcfvb", "anagram":"python"}
 
+def test_anagram_v1():
+    for firstString, secondString in positive_testcases.items():
+        assert anagrams.anagram_v1(firstString,secondString) is True
 
-if __name__ == '__main__':
-    unittest.main()
+    for firstString, secondString in negative_testcases.items():
+        assert anagrams.anagram_v1(firstString,secondString) is False
+
+def test_anagram_v2():
+    for firstString, secondString in positive_testcases.items():
+        assert anagrams.anagram_v2(firstString, secondString) is True
+        
+    for firstString, secondString in negative_testcases.items():
+        assert anagrams.anagram_v2(firstString, secondString) is False 
